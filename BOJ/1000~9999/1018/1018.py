bc38ef871e22b71ff1ad194aa51809748ad46600
HyeonSoo Choi <chs98105@gmail.com>
2020-12-26 07:34:17 +0000

    new: [BOJ] 1018 solving

---
+++ BOJ/1000~9999/1018/1018.py
@@ -0,0 +1,9 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+board = [list(input()) for y in range(M)]  
+print(board)
+
+# https://johnyejin.tistory.com/62
+
+# https://www.acmicpc.net/problem/1018
